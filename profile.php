3978e6ee53253f362ba0648724c12578cd59f0ae
yashpur0hit <yashrpurohit2112@gmail.com>
2024-07-18 18:54:51 +0530

    commit

--- profile.php
+++ profile.php
@@ -1,5 +1,22 @@
 <?php session_start(); ?>
-<?php
-include "con1.php";
-echo "";
-?>
+<?php include "con1.php";
+include "function.php"; 
+include "header.php";?>
+<!-- Start Banner Area -->
+<section class="banner-area organic-breadcrumb">
+    <div class="container">
+        <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+            <div class="col-first">
+                <h1>Your Profile</h1>
+                <nav class="d-flex align-items-center">
+                    <a href="index.php">Home<span class="lnr lnr-arrow-right"></span></a>
+                    <a href="pSayment.php">Profile</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Banner Area -->
+<?php include "Footer.php";?>
+
+
