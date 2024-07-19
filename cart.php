30b877c40ba89985ef554cf8da212f10d1fa6097
yashpur0hit <yashrpurohit2112@gmail.com>
2024-07-19 13:47:16 +0530

    commit

--- cart.php
+++ cart.php
@@ -1,4 +1,4 @@
-<?php session_start();?>
+<?php session_start(); ?>
 <?php include "header.php"; ?>
 <section class="banner-area organic-breadcrumb">
     <div class="container">
@@ -48,12 +48,15 @@ function cart()
 cart();
 ?>
 <!-- ==================Cart Function Ends=================-->
- 
+
 <!--=====================Cart Area =======================-->
 <html>
 
 <head>
     <style>
+        body{
+            background-color: whitesmoke;
+        }
         .cart_img {
             width: 80px;
             height: 80px;
@@ -71,68 +74,83 @@ cart();
         <div class="row">
             <form action="" method="POST" class="w-100">
                 <table class="table table-bordered text-center">
-                    <thead>
-                        <tr>
-                            <th>Product Name</th>
-                            <th>Product Image</th>
-                            <th>Quantity</th>
-                            <th>Total Price</th>
-                            <th>Remove</th>
-                            <th>Operations</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         <?php
                         $get_ip = getIPAddress();
                         $total = 0;
                         $cart_query = "SELECT * FROM `cart` WHERE `IP`='$get_ip'";
                         $result = mysqli_query($con1, $cart_query);
-                        while ($row = mysqli_fetch_array($result)) {
-                            $product_id = $row["Pid"];
-                            $select_products = "SELECT * FROM `product` WHERE `PID`='$product_id'";
-                            $result_product = mysqli_query($con1, $select_products);
-                            while ($row_P = mysqli_fetch_array($result_product)) {
-                                $product_price = $row_P['Price'];
-                                $title = $row_P['Pname'];
-                                $image = $row_P['Pimg'];
-                                // Fetch current quantity in cart
-                                $cart_quantity_query = "SELECT `Quan` FROM `cart` WHERE `Pid`='$product_id' AND `IP`='$get_ip'";
-                                $cart_quantity_result = mysqli_query($con1, $cart_quantity_query);
-                                $cart_quantity_row = mysqli_fetch_assoc($cart_quantity_result);
-                                $cart_quantity = $cart_quantity_row['Quan']; // Default to 1 if not set
+                        $result_count = mysqli_num_rows($result);
+                        if ($result_count > 0) {
+                            echo "<thead>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Image</th>
+                                        <th>Quantity</th>
+                                        <th>Total Price</th>
+                                        <th>Remove</th>
+                                        <th>Operations</th>
+                                    </tr>
+                                </thead>";
+                            while ($row = mysqli_fetch_array($result)) {
+                                $product_id = $row["Pid"];
+                                $select_products = "SELECT * FROM `product` WHERE `PID`='$product_id'";
+                                $result_product = mysqli_query($con1, $select_products);
+                                while ($row_P = mysqli_fetch_array($result_product)) {
+                                    $product_price = $row_P['Price'];
+                                    $title = $row_P['Pname'];
+                                    $image = $row_P['Pimg'];
+                                    // Fetch current quantity in cart
+                                    $cart_quantity_query = "SELECT `Quan` FROM `cart` WHERE `Pid`='$product_id' AND `IP`='$get_ip'";
+                                    $cart_quantity_result = mysqli_query($con1, $cart_quantity_query);
+                                    $cart_quantity_row = mysqli_fetch_assoc($cart_quantity_result);
+                                    $cart_quantity = $cart_quantity_row['Quan']; // Default to 1 if not set
                         
-                                $total_price = $product_price * $cart_quantity;
-                                $total += $total_price;
-                                ?>
-                                <tr>
-                                    <td><?php echo $title ?></td>
-                                    <td><img src="<?php echo $image ?>" alt="" class="cart_img"></td>
-                                    <td>
-                                        <input type="number" name="quan[<?php echo $product_id; ?>]"
-                                            value="<?php echo $cart_quantity; ?>" class="form-input w-50" min="1">
-                                    </td>
-                                    <td><?php echo $product_price ?></td>
-                                    <td><input type="checkbox" name="remove[<?php echo $product_id; ?>]"> Remove</td>
-                                    <td>
-                                        <button type="submit" name="update_cart[<?php echo $product_id; ?>]"
-                                            class="bg-info px-4 py-2 border-1">Update Cart</button>
-                                        <button type="submit" name="discard_cart[<?php echo $product_id; ?>]"
-                                            class="bg-danger px-4 py-2 border-1">Discard</button>
-                                    </td>
-                                </tr>
-                                <?php
+                                    $total_price = $product_price * $cart_quantity;
+                                    $total += $total_price;
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $title ?></td>
+                                        <td><img src="<?php echo $image ?>" alt="" class="cart_img"></td>
+                                        <td>
+                                            <input type="number" name="quan[<?php echo $product_id; ?>]"
+                                                value="<?php echo $cart_quantity; ?>" class="form-input w-50" min="1">
+                                        </td>
+                                        <td><?php echo $product_price ?></td>
+                                        <td><input type="checkbox" name="remove[<?php echo $product_id; ?>]"> Remove</td>
+                                        <td>
+                                            <button type="submit" name="update_cart[<?php echo $product_id; ?>]"
+                                                class="bg-info px-4 py-2 border-1">Update Cart</button>
+                                            <button type="submit" name="discard_cart[<?php echo $product_id; ?>]"
+                                                class="bg-danger px-4 py-2 border-1">Discard</button>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
                             }
+                        } else {
+                            echo "<h2 class='text-center text-danger'>Cart Is Empty</h2>";
                         }
                         ?>
                     </tbody>
                 </table>
             </form>
             <div>
-                <h4 class="px-4">Subtotal:&nbsp;<strong class="text-info">₹&nbsp;<?php echo $total ?></strong></h4>
-                <div class="checkout_btn_inner d-flex align-items-center">
-                    <a class="gray_btn" href="shop1.php">Continue Shopping</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a class="primary-btn" href="payment.php">Proced for Payment</a>
-                </div><br>
+                <?php
+                $get_ip = getIPAddress();
+                $cart_query = "SELECT * FROM `cart` WHERE `IP`='$get_ip'";
+                $result = mysqli_query($con1, $cart_query);
+                $result_count = mysqli_num_rows($result);
+                if ($result_count > 0) {
+                    echo "<h4 class='px-4'>Subtotal:&nbsp;<strong class='text-info'>₹ $total</strong></h4>
+                <div class='checkout_btn_inner d-flex align-items-center'>
+                    <a href='shop1.php'><button class='bg-info border-0 px-3 py-2 mb-2 text-light'>Continue Shopping</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a class='primary-btn' href='payment.php'>Proced for Payment</a>
+                </div><br>";
+                }else{
+                    echo "<a href='shop1.php'><button class='bg-info px-3 py-2 boder-0 mb-2'>Continue Shopping</button></a>";
+                }
+                ?>
             </div>
         </div>
     </div>
